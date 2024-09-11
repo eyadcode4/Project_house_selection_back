095a1c17ea09da4bf2c0f5d061da9f6c7a60f2fd
eyadcode4 <eyadcode24@gmail.com>
2024-09-11 20:29:31 +0200

    initial

---
+++ src/main/java/dec/eyad/house_selection/HouseSelectionApplication.java
@@ -0,0 +1,13 @@
+package dec.eyad.house_selection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseSelectionApplication.class, args);
+	}
+
+}
