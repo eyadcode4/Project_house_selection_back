48962aae4b17f7b25a557c7414685e632704340d
eyadcode4 <eyadcode24@gmail.com>
2024-10-01 18:59:36 +0200

    h2 good

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO consults (id_consult, name, date_consult, location_site, general_behavior) 
 VALUES
 
-(default, 'juan', '2023-04-01', 'Oviedo', 'Depot');
+(default, 'juan', '2023-04-01', 'Oviedo', 'Depot'),
 (default, 'jorge', '2023-04-01', 'Oviedo', 'Depot');
 
