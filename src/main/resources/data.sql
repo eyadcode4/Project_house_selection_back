f3d7b3cca9eca2e5926974ee2e9d7fe74c3c4b5a
eyadcode4 <eyadcode24@gmail.com>
2024-10-02 23:18:44 +0200

    mas in sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,13 @@ INSERT INTO consults (id_consult, name, date_consult, location_site, general_beh
 VALUES
 
 (default, 'juan', '2023-04-01', 'Oviedo', 'Depot'),
-(default, 'jorge', '2023-04-01', 'Oviedo', 'Depot');
+(default, 'jorge', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'maria', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'fefe', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'monica', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'david', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'jose', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'anton', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'amigo', '2023-04-01', 'Oviedo', 'Depot'),
+(default, 'amiga', '2023-04-01', 'Oviedo', 'Depot');
 
